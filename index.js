33a7ccdabe1632cc510dc01446fd38184970d27b
Jason Lestina <coda19@gmail.com>
2016-08-16 14:06:22 -0400

    added basic express server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('whoooooooooooooooooooooooooooooooooooooooooo')
+var express = require('express')
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(80, function () {
+  console.log('Example app listening on port 80!');
+});
