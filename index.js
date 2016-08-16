d0f654f4b46a82f342df9f20e4a21dcda922392c
Jason Lestina <coda19@gmail.com>
2016-08-16 14:26:02 -0400

    more server file revisions

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ var app = express();
 app.set('port', (process.env.PORT || 8080))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'egs')
-app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (request, response) {
   response.send('Hello World!');
 });
+
+app.use(express.static(__dirname + '/public'))
 
 app.listen(app.get('port'), function () {
   console.log('Example app listening on port', app.get('port'));
