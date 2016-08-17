697a4fb28eee99a4b73ffb1db24a397433f5779d
Jason Lestina <coda19@gmail.com>
2016-08-17 16:38:51 -0400

    adding api routes

--- index.js
+++ index.js
@@ -11,84 +11,72 @@ app.set('port', (process.env.PORT || 8080))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 
-app.get('/install', function(request, response){
-  // Create the users table
-  knex.schema.createTable('users', function (table) {
-    table.increments('id')
-    table.string('username')
-    table.string('email')
-    table.string('password')
-    table.string('avatar')
-    table.string('bio')
-    table.integer('karma')
-  })
-    .then(function(){
-      console.log('Created users table')
-    })
+app.get('/', function (request, response) {
 
-  // Create the teams table
-  knex.schema.createTable('teams', function (table) {
-    table.increments('id')
-    table.integer('game_id')
-    table.integer('seriousness')
-    table.string('description')
-    table.boolean('access')
-    table.uuid('invite')
-    table.integer('size')
-    table.integer('mode_id')
-  })
-    .then(function(){
-      console.log('Created teams table')
-    })
-
-  // Create the games table
-  knex.schema.createTable('games', function (table) {
-    table.increments('id')
-    table.string('title')
-    table.string('cover')
-  })
-    .then(function(){
-      console.log('Created games table')
-    })
-
-  // Create the roles table
-  knex.schema.createTable('roles', function (table) {
-    table.increments('id')
-    table.string('name')
-    table.string('icon')
-    table.integer('game_id')
-  })
-    .then(function(){
-      console.log('Created roles table')
-    })
-
-  // Create the modes table
-  knex.schema.createTable('modes', function (table) {
-    table.increments('id')
-    table.string('name')
-    table.integer('game_id')
-  })
-    .then(function(){
-      console.log('Created modes table')
-    })
-
-  // Create the friends table
-  knex.schema.createTable('friends', function (table) {
-    table.increments('id')
-    table.integer('user_id')
-    table.integer('friend_id')
-  })
-    .then(function(){
-      console.log('Created friends table')
-    })
-
-  response.send('Finished')
 })
 
-app.get('/', function (request, response) {
-  var loggedIn = request.query.loggedin === 'yes'
+// ROUTES
+app.get('/api/v1/users', function(req, res){
+  knex
+    .select()
+    .from('users')
+    .then(function(data){
+      res.json(data)
+    })
+})
 
-  response.render('template', {loggedIn: loggedIn})
+app.get('/api/v1/teams', function(req, res){
+  knex
+    .select()
+    .from('teams')
+    .then(function(data){
+      res.json(data)
+    })
+})
+
+app.get('/api/v1/games', function(req, res){
+  knex
+    .select()
+    .from('games')
+    .then(function(data){
+      res.json(data)
+    })
+})
+
+app.get('/api/v1/modes', function(req, res){
+  knex
+    .select()
+    .from('modes')
+    .then(function(data){
+      res.json(data)
+    })
+})
+
+app.get('/api/v1/roles', function(req, res){
+  knex
+    .select()
+    .from('roles')
+    .then(function(data){
+      res.json(data)
+    })
+})
+
+app.get('/api/v1/user_team', function(req, res){
+  knex
+    .select()
+    .from('user_team')
+    .then(function(data){
+      res.json(data)
+    })
+})
+
+app.get('/api/v1/friends', function(req, res){
+  knex
+    .select()
+    .from('friends')
+    .then(function(data){
+      res.json(data)
+    })
 })
 
 app.use(express.static(__dirname + '/public'))
