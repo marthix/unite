e8b2e404d27a7cfb8e6e95fc050a0a0e17d1f991
Jason Lestina <coda19@gmail.com>
2016-08-17 16:42:06 -0400

    fixed get response for home

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.set('port', (process.env.PORT || 8080))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 
-app.get('/', function (request, response) {
-
+app.get('/', function (req, res) {
+  res.render('index')
 })
 
 // ROUTES
