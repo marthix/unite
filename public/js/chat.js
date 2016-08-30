39816979669b8edcb43a87244040a506caa2f229
Jason Lestina <coda19@gmail.com>
2016-08-30 18:20:42 -0400

    realtime chat works whooooo

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,41 +1,119 @@
-var teamId = window.location.search.replace('?id=', '')
-
-fetch('/api/v1/teams?id=' + teamId, {
-  credentials: 'include'
+// Pusher to send chats
+channel.bind('message', function(data) {
+  sendChat(data)
 })
-  .then(function(response) {
-    return response.json()
-  })
 
-  //Take the JSON object, and begin creating HTML elements
-  .then(function(json){
-    console.log(json)
+//Setup the structure for chatting
+var chatContainer = document.createElement('div')
+chatContainer.classList.add('column', 'column-25')
+chatContainer.setAttribute('id', 'chat-box')
 
-    var chatContainer = document.getElementById('chat-box')
+var chatBox = document.createElement('div')
+chatBox.classList.add('chat')
 
-    var chatBox = document.createElement('div')
-    chatBox.classList.add('chat')
+var chatList = document.createElement('ul')
+chatList.setAttribute('id', 'chat-list')
 
-    var chatInputBox = document.createElement('div')
-    chatInputBox.classList.add('chat-input-box')
+var chatInputBox = document.createElement('div')
+chatInputBox.classList.add('chat-input-box')
 
-    var chatInput = document.createElement('input')
-    chatInput.classList.add('chat-input')
+var chatInput = document.createElement('textarea')
+chatInput.classList.add('chat-input')
+chatInput.setAttribute('maxlength', '200')
 
-    var chatButton = document.createElement('button')
-    chatButton.classList.add('button', 'chat-button')
-    chatButton.innerHTML = 'SEND'
+var chatButton = document.createElement('button')
+chatButton.classList.add('button', 'chat-button')
+chatButton.innerHTML = 'SEND'
 
-    chatInputBox.appendChild(chatInput)
-    chatInputBox.appendChild(chatButton)
+chatBox.appendChild(chatList)
 
-    chatContainer.appendChild(chatBox)
-    chatContainer.appendChild(chatInputBox)
-  })
+chatInputBox.appendChild(chatInput)
+chatInputBox.appendChild(chatButton)
 
+chatContainer.appendChild(chatBox)
+chatContainer.appendChild(chatInputBox)
 
-Pusher.logToConsole = true;
+document.getElementById('team-lobby').appendChild(chatContainer)
 
-channel.bind('chat', function(data) {
-
+// Event listener for typing
+chatInput.addEventListener('keypress', function(e) {
+  if(e.keyCode === 13 && chatInput.value != '') {
+    e.preventDefault()
+    fetchAPI(chatInput.value)
+  }
 })
+
+document.body.addEventListener('click', function(e) {
+  if(e.target === chatButton) {
+    chatButton.blur()
+  }
+
+  if(e.target === chatButton && chatInput.value != '') {
+    var message = chatInput.value
+    fetchAPI(message)
+  }
+})
+
+
+// Utility functions
+function fetchAPI (message) {
+  fetch('/api/v1/chat', {
+    credentials: 'include',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      team_id: window.location.search.replace('?id=', ''),
+      message: message,
+      username: user.username_full,
+      avatar: user.avatar,
+      discord_id: user.discord_id
+    })
+  })
+}
+
+function sendChat (data) {
+  var chatList = document.getElementById('chat-list')
+
+  var chatItem = document.createElement('li')
+  chatItem.classList.add('chat-item', 'row')
+
+  var avatarContainer = document.createElement('div')
+  avatarContainer.classList.add('column', 'column-20')
+
+  var avatar = document.createElement('img')
+  avatar.classList.add('avatar')
+  avatar.setAttribute('src', 'https://discordapp.com/api/users/' + data.discord_id + '/avatars/' + data.avatar + '.jpg')
+  avatar.setAttribute('width', '100%')
+
+  var messageContainer = document.createElement('div')
+  messageContainer.classList.add('column', 'column-80')
+
+  var name = document.createElement('p')
+  name.classList.add('chat-name')
+  name.innerHTML = data.username
+
+  var message = document.createElement('p')
+  message.classList.add('chat-message')
+  message.innerHTML = data.message
+
+  avatarContainer.appendChild(avatar)
+  messageContainer.appendChild(name)
+  messageContainer.appendChild(message)
+
+  chatItem.appendChild(avatarContainer)
+  chatItem.appendChild(messageContainer)
+
+  chatList.appendChild(chatItem)
+  scrollChat()
+  clearChat()
+}
+
+function clearChat () {
+  chatInput.value = ''
+}
+
+function scrollChat () {
+  chatBox.scrollTop = chatBox.scrollHeight
+}
